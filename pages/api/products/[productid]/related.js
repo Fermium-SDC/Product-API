00ec3b6e4d09e24be541850a91ee1f0b69a81936
Patrick O'Shea <osheap81796@gmail.com>
2022-07-28 16:43:01 -0400

    Added CORS, products and related APIs working

---
+++ pages/api/products/[productid]/related.js
@@ -0,0 +1,20 @@
+import sql from "../../../../backend/db";
+
+export default async function getRelatedProducts(req, res) {
+  if (req.method !== "GET") {
+    res.status(500).send("GET requests only");
+    return;
+  }
+  const product_id = req.query.productid;
+  try {
+    const products = await sql`
+    SELECT
+    related_id
+    FROM "Related Products"
+    WHERE product_id = ${product_id};
+  `.values()
+    res.json(products.map(row => row[0]));
+  } catch (error) {
+    res.status(500).send("Error fetching data ", error);
+  }
+}
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -5,12 +5,15 @@ export default async function getProducts(req, res) {
     res.status(500).send("GET requests only");
     return;
   }
+  const pageNum = req.query.page || 1;
+  const perPage = req.query.count || 5;
   try {
     const products = await sql`
       SELECT
       *
       FROM "Products"
-      WHERE product_id = 1
+      ORDER BY product_id ASC LIMIT ${perPage}
+      OFFSET ${pageNum * perPage - perPage}
     `;
     res.json(products);
   } catch (error) {
