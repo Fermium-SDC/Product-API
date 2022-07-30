0b0f4b298642cbe6cd6a428af64e0e652e4081c9
Patrick O'Shea <osheap81796@gmail.com>
2022-07-29 20:24:22 -0400

    Minor change to styles query

--- pages/api/products/[productid]/styles.js
+++ pages/api/products/[productid]/styles.js
@@ -8,9 +8,6 @@ export default async function getProductStyles(req, res) {
   const product_id = req.query.productid;
   try {
     const styles = await sql`
-    SELECT
-      p.product_id,
-      (
         SELECT json_agg(styles)
         FROM (
         SELECT
@@ -44,16 +41,13 @@ export default async function getProductStyles(req, res) {
         FROM "Styles" s
         WHERE s.product_id = ${product_id}
         ) AS styles
-      ) AS "results"
-    FROM "Products" p
-    WHERE product_id = ${product_id}
     `;
 
     //if (styles[0].json_build_object.results === null) {
     //  styles[0].json_build_object.results = [];
     //}
 
-    res.json(styles[0]);
+    res.json({ product_id: product_id, results: styles[0].json_agg });
   } catch (error) {
     res.status(500).send("Error fetching data ", error);
   }
